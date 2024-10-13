df5273efca41d6c9add5e65ac667ecf5a153143c
Devon Casto <dlcasto4@gmail.com>
2024-10-13 15:04:25 -0400

    Update setnavandfooter.js

--- assets/text/js/setnavandfooter.js
+++ assets/text/js/setnavandfooter.js
@@ -4,7 +4,6 @@ function updateNavAndFooter() {
     navm.innerHTML = `
         <button class="navbtn" onclick="window.location.href='/';"><i class="wicon wicon-home"></i><br><span class="navtext">Home</span></button>
         <div class="right">
-            <button class="navbtn" onclick="window.location.href='/chat/';"><i class="wicon wicon-chat"></i><br><span class="navtext">Chat</span></button>
             <button class="navbtn" onclick="window.location.href='/axis/';"><i class="wicon wicon-axis"></i><br><span class="navtext">Axis</span></button>
             <button class="navbtn" onclick="window.location.href='/games/';"><i class="wicon wicon-gamepad-handles"></i><br><span class="navtext">Games</span></button>
             <button class="navbtn" onclick="window.location.href='/tools/';"><i class="wicon wicon-tools"></i><br><span class="navtext">Tools</span></button>
@@ -15,7 +14,6 @@ function updateNavAndFooter() {
     navs.innerHTML = `
         <div style="height: 55px; width: 100%;"></div> <!-- spacer -->
         <button onclick="window.location.href='/';"><i class="wicon wicon-home"></i><span>Home</span></button>
-        <button onclick="window.location.href='/chat/';"><i class="wicon wicon-chat"></i><span>Chat</span></button>
         <button onclick="window.location.href='/axis/';"><i class="wicon wicon-axis"></i><span>Axis</span></button>
         <button onclick="window.location.href='/games/';"><i class="wicon wicon-gamepad-handles"></i><span>Games</span></button>
         <button onclick="window.location.href='/tools/';"><i class="wicon wicon-tools"></i><span>Tools</span></button>
@@ -33,12 +31,7 @@ function updateNavAndFooter() {
             <div class="leftwrapper">
                 <div class="section">
                     <p>Personal links</p>
-                    <a href="https://github.com/wraithcode2018"><i class="wicon wicon-github"></i>&nbsp;Github</a>
-                    <a href="https://app.revolt.chat"><span class="wicon wicon-revoltchat"></span>&nbsp; Revolt</a>
-                    <a href="https://www.reddit.com/user/wraithcode18/"><i class="wicon wicon-reddit"></i>&nbsp;Reddit</a>
-                    <a href="https://youtube.com/@wraithcoding"><i class="wicon wicon-youtube"></i>&nbsp;Youtube</a>
-                    <a href="https://open.spotify.com/user/31ssxskpd7xxb24mhzvbwlwgnv6u"><i class="wicon wicon-spotify"></i>&nbsp;Spotify</a>
-                    <a href="https://wraithcode.straw.page"><i class="wicon wicon-strawpage"></i>&nbsp;Straw.page</a>
+                    <a href="https://github.com/dlcasto4"><i class="wicon wicon-github"></i>&nbsp;Github</a>
                 </div>
                 <div class="section">
                     <p>Navigation</p>
