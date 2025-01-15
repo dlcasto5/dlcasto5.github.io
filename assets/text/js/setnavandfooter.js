9e9ff142cf8c2808a6eb9057d7a5577a342e6ec7
Devon Casto <dlcasto4@gmail.com>
2025-01-15 16:39:01 -0500

    Update setnavandfooter.js

--- assets/text/js/setnavandfooter.js
+++ assets/text/js/setnavandfooter.js
@@ -6,16 +6,16 @@ function updateNavAndFooter() {
         <div class="right">
             <button class="navbtn" onclick="window.location.href='/axis/';"><i class="wicon wicon-axis"></i><br><span class="navtext">Axis</span></button>
             <button class="navbtn" onclick="window.location.href='/games/';"><i class="wicon wicon-gamepad-handles"></i><br><span class="navtext">Games</span></button>
-            <button onclick="window.location.href='/apps/';"><span>apps</span></button>
+            <button onclick="window.location.href='/apps/';"><br><span class="navtext">apps</span></button>
         </div>
     `;
     const navs = document.getElementById("sidenav");
     navs.innerHTML = `
         <div style="height: 55px; width: 100%;"></div> <!-- spacer -->
-        <button onclick="window.location.href='/';"><i class="wicon wicon-home"></i><span>Home</span></button>
-        <button onclick="window.location.href='/axis/';"><i class="wicon wicon-axis"></i><span>Axis</span></button>
-        <button onclick="window.location.href='/games/';"><i class="wicon wicon-gamepad-handles"></i><span>Games</span></button>
-        <button onclick="window.location.href='/apps/';"><span>apps</span></button>
+        <button onclick="window.location.href='/';"><i class="wicon wicon-home"></i><br><span class="navtext">Home</span></button>
+        <button onclick="window.location.href='/axis/';"><i class="wicon wicon-axis"></i><br><span class="navtext">Axis</span></button>
+        <button onclick="window.location.href='/games/';"><i class="wicon wicon-gamepad-handles"></i><br><span class="navtext">Games</span></button>
+        <button onclick="window.location.href='/apps/';"><br><span class="navtext">apps</span></button>
     `;
             
     const footer = document.getElementById("footer");
